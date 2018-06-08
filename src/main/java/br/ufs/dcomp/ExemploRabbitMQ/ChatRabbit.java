cd07a067e7a79aaf5f36b921ed5c20468908a60b
Accel <accelwo@dcomp.ufs.br>
2018-06-08 22:02:45 +0000

    criado ChatRabbit

---
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/ChatRabbit.java
@@ -0,0 +1,48 @@
+package br.ufs.dcomp.ExemploRabbitMQ;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class ChatRabbit {
+
+  private final static String QUEUE_NAME = "minha-fila";
+
+  public static void main(String[] argv) throws Exception {
+    Scanner entrada = new Scanner(System.in);
+    
+    
+    ConnectionFactory factory = new ConnectionFactory();
+    //factory.setUri("amqp://huarumck:VncxT9rNIpuDuLcCkfJqne0JWAlKbA0k@otter.rmq.cloudamqp.com/huarumck");
+    factory.setHost("ec2-34-220-41-87.us-west-2.compute.amazonaws.com");
+    factory.setUsername("accelwo");
+    factory.setPassword("meupass");
+    factory.setVirtualHost("/");
+    
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    System.out.println(" [*] Esperando recebimento de mensagens...");
+
+    Consumer consumer = new DefaultConsumer(channel) {
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+          throws IOException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Mensagem recebida: '" + message + "'");
+      }
+    };
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+    for (int i = 0; i < 10; i++){
+        System.out.println("loop "+ i);
+    }
+    
+    System.out.print("Digite sua mensage: ");
+    String message = entrada.nextLine();
+    //String message = "Olá!!!";
+    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Mensagem enviada: '" + message + "'");
+  }
+}
