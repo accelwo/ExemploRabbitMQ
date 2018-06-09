966d3f6a0daf36afca6c9f41204b18c3f905a173
Accel <accelwo@dcomp.ufs.br>
2018-06-09 15:29:38 +0000

    Adição imcompleta do chat

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/ChatRabbit.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/ChatRabbit.java
@@ -13,6 +13,9 @@ public class ChatRabbit {
 
   private final static String QUEUE_NAME = "minha-fila";
   private final static DateFormat sdf = new SimpleDateFormat("'('dd/MM/yyyy 'às' HH:mm:ss') '");
+  public static boolean novo_login;
+  public static boolean novo_destino;
+  public static String Destino = new String("");
 
   public static void main(String[] argv) throws Exception {
     Scanner entrada = new Scanner(System.in);
@@ -29,33 +32,77 @@ public class ChatRabbit {
     
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
+    
+    System.out.print("Usuário: ");
+    String user = entrada.nextLine();
+    
+    System.out.println("Destino =" + Destino);
+    
+    System.out.println("Bem vindo " + user);
+    novo_login = true;
+    //System.out.print(">>");
 
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    System.out.println(" [*] Esperando recebimento de mensagens...");
+    channel.queueDeclare(user, false, false, false, null);
+    System.out.println(" [*] Carregando Mensagens salvas!  [*]");
 
     Consumer consumer = new DefaultConsumer(channel) {
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
           throws IOException {
         String message = new String(body, "UTF-8");
         //System.out.println(" [x] Mensagem recebida: '" + message + "'");
-        System.out.println(message);
+        if (novo_login) {
+          System.out.println(message);
+          System.out.println("novo");
+        } else {
+          System.out.println("");
+          System.out.println(message);
+          System.out.print(Destino + ">>");
+        }
       }
     };
-    channel.basicConsume(QUEUE_NAME, true, consumer);
+    channel.basicConsume(user, true, consumer);
+    
+    System.out.println(" [*] Finalizando mensagens salvas! [*]");
+    novo_login = false;
+
+    
     for (int i = 0; i <= 5; i++){
         //System.out.println("loop "+ i);
     
     
-    System.out.print("Digite sua menssage: ");
+    //System.out.print("Digite sua menssage: ");
+    System.out.print(Destino + ">>");
     String message = entrada.nextLine();
+    
+    //System.out.println("primeiro Char = "+ message.substring(0,1));
+    if (message.substring(0,1).equals("@")){
+      //System.out.println("Recebida @");
+      Destino = message.substring(1,message.length());
+      //System.out.println("Destinatario : " + Destino);
+      novo_destino = true;
+    }
     Date data = new Date();
 
-    System.out.println("Data  = "+ data);
-    System.out.println("Data2 = "+ sdf.format(data));
-    String texto = sdf.format(data) + QUEUE_NAME + " diz: " + message; 
+    //System.out.println("Data  = "+ data);
+    //System.out.println("Data2 = "+ sdf.format(data));
+    String texto = sdf.format(data) + user + " diz: " + message; 
     
     //String message = "Olá!!!";
-    channel.basicPublish("", QUEUE_NAME, null, texto.getBytes("UTF-8"));
+    if (novo_destino){
+      //System.out.println("");
+      novo_destino = false;
+    } else {
+      if (!Destino.equals("")){
+        channel.basicPublish("", Destino, null, texto.getBytes("UTF-8"));  
+        //System.out.println("Enviado: "+ texto);
+        
+      } else {
+        System.out.println("[*] Erro: Nenhum destinatario selecionado  [*]");
+        System.out.println("[*] Por favor utilize o comando @nome para [*]");
+        System.out.println("[*] selecionar o destinatario              [*]");
+      }
+    }
+    
     //System.out.println(" [x] Mensagem enviada: '" + message + "'");
     }
   }
